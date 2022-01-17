3a1032f93645f784ef56ba0c79da0562655ea103
karthikpkapture <karthik.p@kapturecrm.com>
2022-01-17 12:59:48 +0530

    API data

--- src/components/Data.js
+++ src/components/Data.js
@@ -24,6 +24,7 @@ import axios from 'axios';
 function Data() {
 
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const [users, setUsers] = React.useState([]);
 
     const handleClick = (event) => {
       setAnchorEl(event.currentTarget);
@@ -46,15 +47,11 @@ function Data() {
 
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 90,
-        },
+        { field: 'id', headerName: 'ID' },
+        { field: 'email', headerName: 'Email', width: 200 },
+        { field: 'first_name', headerName: 'First name', width: 130 },
+        { field: 'last_name', headerName: 'Last name', width: 130 },
+       
         {
           field: 'fullName',
           headerName: 'Full name',
@@ -62,34 +59,23 @@ function Data() {
           sortable: false,
           width: 160,
           valueGetter: (params) =>
-            `${params.getValue(params.id, 'firstName') || ''} ${
-              params.getValue(params.id, 'lastName') || ''
+            `${params.getValue(params.id, 'first_name') || ''} ${
+              params.getValue(params.id, 'last_name') || ''
             }`,
         },
       ];
       
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 31 },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-      ];
-
       useEffect(() => {
 
         async function user(){
             const request =await axios.get(`https://reqres.in/api/users?page=2`)
-                console.log(request.data);
-               
+                // console.log(request.data.data);
+                setUsers(request.data.data);
+                 
         }
         user();
-
-    }, [])
+        console.log(users);
+    }, []); 
 
       
     return (
@@ -106,7 +92,7 @@ function Data() {
               <div style={{ height: 600, width: '80%', alignItems:'center', justifyContent:'center', margin:'auto' }}>
               
                 <DataGrid
-                    rows={rows}
+                    rows={users}
                     columns={columns}
                     pageSize={10}
                     rowsPerPageOptions={[5]}
